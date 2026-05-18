c4a17f635145d2b9d58a12f26d8ba0042514f0c2
Azazel-Robotics <camille.malazarte516@gmail.com>
2026-05-18 10:00:38 -0400

    Reapply "attempt one at making a testing periodic routine"
    This reverts commit 604d2b8b669c6db8ee4dd2d76234dbabbe3f0cdb.

--- src/main/java/frc/robot/generated/TunerConstants.java
+++ src/main/java/frc/robot/generated/TunerConstants.java
@@ -76,7 +76,7 @@ public class TunerConstants {
     // This needs to be tuned to your individual robot
     public static final LinearVelocity kSpeedAt12Volts = MetersPerSecond.of(5.12);
 
-    // Every 1 rotation of the azimuth results in kCoupleRatio drive motor turns;
+    // Every 1 rotation of the azimuth (steering motor) results in kCoupleRatio drive motor turns;
     // This may need to be tuned to your individual robot
     private static final double kCoupleRatio = 3.857142857142857;
 
@@ -175,6 +175,7 @@ public class TunerConstants {
             kFrontLeftSteerMotorId, kFrontLeftDriveMotorId, kFrontLeftEncoderId, kFrontLeftEncoderOffset,
             kFrontLeftXPos, kFrontLeftYPos, kInvertLeftSide, kFrontLeftSteerMotorInverted, kFrontLeftEncoderInverted
         );
+
     public static final SwerveModuleConstants<TalonFXConfiguration, TalonFXConfiguration, CANcoderConfiguration> FrontRight =
         ConstantCreator.createModuleConstants(
             kFrontRightSteerMotorId, kFrontRightDriveMotorId, kFrontRightEncoderId, kFrontRightEncoderOffset,
